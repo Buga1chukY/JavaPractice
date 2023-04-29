fa13945835a1bb3d3b0a306cc64ef05d031b8b37
Yaroslav Buhaichuk <yaroslav1415@gmail.com>
2023-04-30 00:19:24 +0300

    [OOP] Practice OOP

---
+++ src/main/oop_practice/animal_practice/AnimalMain.java
@@ -0,0 +1,14 @@
+package main.oop_practice.animal_practice;
+
+public class AnimalMain {
+    public static void main(String[] args) {
+        Dog dog = new Dog("Bob");
+        dog.sound();
+        dog.dogName();
+
+
+        Dog dog2 = new Dog("Rob");
+        //dog2.name = "James";
+        dog2.dogName();
+    }
+}
