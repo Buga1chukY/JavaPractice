9af4e080a95cd50a6df1d231dc692756033dcd0a
Yaroslav Buhaichuk <yaroslav1415@gmail.com>
2023-09-17 23:32:35 +0300

    [Stack] Implement stack class

---
+++ src/main/Stack.java
@@ -0,0 +1,73 @@
+package main;
+
+public class Stack {
+    
+    private final static int INITIAL_SIZE = 10;
+
+    private int countElement = 0;
+    private int[] dynamicArray = new int[INITIAL_SIZE];
+
+    /**
+     * AddElementFunction
+     */
+    public void addElemnt(int element) {
+
+        int arraySize = dynamicArray.length;
+        if (arraySize == countElement) {
+
+            int newSize = arraySize * 2;
+            int[] newArray = new int[newSize];
+
+            for (int j = 0; j < dynamicArray.length; j++) {
+                newArray[j] = dynamicArray[j];
+            }
+            dynamicArray = newArray;
+            dynamicArray[countElement] = element;
+            countElement++;
+        } else {
+            dynamicArray[countElement] = element;
+            countElement++;
+        }
+    }
+
+    /**
+     * SubtractElementFunction
+     */
+    public int subtractElement() {
+        int lastElementIndex = countElement - 1;
+        int lastElement = dynamicArray[lastElementIndex];
+        dynamicArray[lastElementIndex] = 0;
+        countElement--;
+
+        int arraySize = dynamicArray.length;
+        int emptyPlaces = arraySize - countElement;
+        int half = arraySize / 2;
+
+        if (emptyPlaces >= half) {
+            int[] newArray = new int[half];
+
+            for (int i = 0; i < countElement; i++) {
+                newArray[i] = dynamicArray[i];
+            }
+            dynamicArray = newArray;
+        }
+
+        return lastElement;
+    }
+    
+    /**
+     * PrintArrayFunction
+     */
+    public void print() {
+        StringBuilder output = new StringBuilder();
+
+        for (int i = 0; i < countElement; i++) {
+            output.append(dynamicArray[i]);
+
+            if (i != countElement - 1) {
+                output.append(", ");
+            }
+        }
+        System.out.println(output);
+    }
+}
